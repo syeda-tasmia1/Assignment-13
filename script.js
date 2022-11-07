b66433997677e0e65af2aa356a861f9eaf4f13ca
syeda-tasmia1 <113660745+syeda-tasmia1@users.noreply.github.com>
2022-11-07 18:04:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// Task1
+var value = 1;
+
+console.log(value);
+
+
+// Task2
+const myName = 'tasmia';
+
+console.log(myName);
+
+
+// Task3
+let X = 3;
+let Y = 4;
+
+let Z = X + Y;
+
+console.log(Z);
+
+
+// Task4
+let myColor = 'purple';
+
+console.log(myColor);
+
+
+// Task5
+let fName = 'Lucy', lName = 'John', age = 25;
+
+console.log(fName);
+console.log(lName);
+console.log(age);
